77501accae45ce337df400c8350460961936f3ab
Nick Herrick <nth.herrick@gmail.com>
2019-10-27 12:39:20 -0400

    added necessary html, added css styling, added psuedocode to javascript file

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+// Load html first
+
+$(document).ready( function() {
+
+    // Declare Variables
+
+    // Start the game on a button click
+
+    // Display question 1 with timer
+
+    // Determine whether answer was correct, incorrect, or time limit was reached
+
+    // Log the results into a scorekeeper variable
+
+    // Display next question, rinse and repeat
+
+    // After last question, display final score along with a restart button
+
+
+
+
+});
