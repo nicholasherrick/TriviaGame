bb2b75f12a68b39a33d0c6b685300d23652c2be9
Nick Herrick <nth.herrick@gmail.com>
2019-10-27 12:58:49 -0400

    added javascript functionality to start game button. clicking will hide the button, start the game, and reveal the html game content

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,16 +13,23 @@ $(document).ready( function() {
 
 
     // Start the game on a button click
+    $("#start-button").on("click", function (startGame) {
+        $("#start-button").hide();
+        $("#time-remaining").show();
+        $("#question").show();
+        $("#answers").show();
 
-    // Display question 1 with timer
+        // Display question 1 with timer
 
-    // Determine whether answer was correct, incorrect, or time limit was reached
+        // Determine whether answer was correct, incorrect, or time limit was reached
 
-    // Log the results into a scorekeeper variable
+        // Log the results into a scorekeeper variable
 
-    // Display next question, rinse and repeat
+        // Display next question, rinse and repeat
 
-    // After last question, display final score along with a restart button
+        // After last question, display final score along with a restart button
+
+    });
 
 
 
