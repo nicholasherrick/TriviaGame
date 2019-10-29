0873850e0276d58773d358abb8b96c8ff7653e87
Nick Herrick <nth.herrick@gmail.com>
2019-10-29 18:53:48 -0400

    added questions object array to new javascript file

---
+++ assets/javascript/app.js
@@ -0,0 +1,46 @@
+$(document).ready( function() {
+
+    var questions = [
+        {
+            question1: {
+                question: "1. What is 2 + 2?",
+                answer1: "4",
+                answer2: "0",
+                answer3: "8",
+                answer4: "2",
+            },
+            question2: {
+                question: "2. What is 9 * 0?",
+                answer1: "9",
+                answer2: "-9",
+                answer3: "0",
+                answer4: "18",
+            },
+            question3: {
+                question: "3. What is 17 - 21??",
+                answer1: "-6",
+                answer2: "6",
+                answer3: "-4",
+                answer4: "4",
+            },
+            question4: {
+                question: "4. What is 4 / .5?",
+                answer1: "4",
+                answer2: "16",
+                answer3: "12",
+                answer4: "8",
+            },
+            question5: {
+                question: "5. What is 164 - 112?",
+                answer1: "84",
+                answer2: "52",
+                answer3: "48",
+                answer4: "62",
+            },
+        },
+    ]
+
+
+
+
+});
